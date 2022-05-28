88d2b901c338474a28b359d9b0898a1ddfbd25cb
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:12:12 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs19.js
@@ -0,0 +1,18 @@
+function tugas19a() {
+    var kata = "semangat...";
+    console.log(kata.repeat(5));
+}
+function tugas19b() {
+    var kata = "*****";
+    console.log(kata);
+    console.log(kata.repeat(5));
+    console.log(kata.repeat(10));
+    console.log(kata.repeat(15));
+    console.log(kata.repeat(20));
+    console.log(kata.repeat(15));
+    console.log(kata.repeat(10));
+    console.log(kata.repeat(5));
+    console.log(kata);
+}
+tugas19a();
+tugas19b();
